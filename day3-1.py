58bcecc5b6b4e590f6949990646711db1613fa99
pikirk <paul.kirk@dteenergy.com>
2019-12-07 17:23:47 -0800

    calcuate set intserction of lines

--- day3-1.py
+++ day3-1.py
@@ -1,3 +1,5 @@
+import copy
+
 def nextPoint (origin, dir, len):
     if dir == 'R':
         return [(origin[0] + len), origin[1]]
@@ -17,9 +19,67 @@ def hitTest(pt1, pt2):
 def distanceToPort(pt):
     return abs(pt[0]) + abs(pt[1])
 
-def readAndParse():
-    # TODO input = open("day3-1-input.txt", "r").readlines()
-    return [ [{}], [{}] ]
+# ex. R456,U344,D4566
+def parse(inputs):
+    retValue = []
+    for i in range( len(inputs) ):
+        s = inputs[i]
+        retValue.append( list( (s.split(',') ) ) )
+    return retValue
+
+# ex. R456
+def parseSpec(spec):
+    dir = spec[0]
+    take = len(spec)
+    size = spec[1:take] 
+    return [dir, int (size.strip())]
+
+# # plots both lines based on the direction specs and
+# # evaluates if lines intersect during the plot
+# # we only have 2 lines to worry about for this puzzle
+# def plotAndEval(paths):
+#     retValue = []
+
+#     ob = 0
+#     if len(paths[0]) > len(paths[1]):                       # determine max outer boundary
+#         ob = len(paths[0])
+#     else: 
+#         ob = len(paths[1]) 
+
+#     origin1 = [0,0] 
+#     origin2 = [0,0]
+
+#     for curIndex in range(ob):
+#         first = parseSpec(paths[0][curIndex])               # plot first line
+#         pt1 = nextPoint(origin1, first[0], first[1])
+#         origin1 = pt1
+                                            
+#         if ( curIndex <= len(paths[1]) ):                   # plot second line
+#             second = parseSpec(paths[1][curIndex])  
+#             pt2 = nextPoint(origin2, second[0], second[1])
+#             origin2 = pt2
+
+#         if (hitTest(pt1, pt2)):
+#             retValue.append(pt1)
+    
+#     return 
+
+def plotPoints(path):
+    line = set()
+    origin = [0,0]
+    for index in range(len(path)):
+        spec = parseSpec(path[index])
+        next = nextPoint(origin, spec[0], spec[1])
+        line.add(str(next[0]) + ',' + str(next[1]))
+        origin = next
+    return line
+
+def plotAndEval(paths):
+    firstSet = plotPoints(paths[0])
+    secondSet = plotPoints(paths[1])
+    diff = firstSet.intersection(secondSet)   # get the common points in poth sets
+    print(diff)
+
 
 # outline
 # read and parse input
@@ -29,7 +89,9 @@ def readAndParse():
 # get the shortest distance from origin (0,0) for all intersections
 
 # test inputs
-pinkPath = ['R1009','U263','L517','U449','L805','D78','L798','D883'',L777','D562']
-bluePath = ['L1003','D960','L10','D57','R294','U538','R867','D426','L524','D441','R775','U308']
+inputs = open('test.txt', 'r').readlines()
+result = parse(inputs)
+plotAndEval(result)
+
 
 
